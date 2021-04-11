14d28609dd772588acc2c315838dfb47107660fa
Louis-Axel Ambroise <ambroise.la@pm.me>
2021-04-11 18:56:50 +0200

    Update SimpleEventQuery.cs

--- FasTnT.Application/Services/Queries/SimpleEventQuery.cs
+++ FasTnT.Application/Services/Queries/SimpleEventQuery.cs
@@ -8,7 +8,6 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -16,51 +15,6 @@ namespace FasTnT.Application.Queries.Poll
 {
     public class SimpleEventQuery : IEpcisQuery
     {
-        private static readonly IDictionary<string, Func<IQueryable<Event>, QueryParameter, IQueryable<Event>>> SimpleParameters = new Dictionary<string, Func<IQueryable<Event>, QueryParameter, IQueryable<Event>>>
-        {
-            { "eventType",               (query, param) => query.Where(x => param.Values.Select(Enumeration.GetByDisplayName<EventType>).Contains(x.Type)) },
-            { "eventCountLimit",         (query, param) => query.Take(param.GetValue<int>()) },
-            { "maxEventCount",           (query, param) => query.Take(param.GetValue<int>() + 1) },
-            { "GE_eventTime",            (query, param) => query.Where(x => x.EventTime >= param.GetValue<DateTime>()) },
-            { "LT_eventTime",            (query, param) => query.Where(x => x.EventTime < param.GetValue<DateTime>()) },
-            { "GE_recordTime",           (query, param) => query.Where(x => x.Request.CaptureDate >= param.GetValue<DateTime>()) },
-            { "LT_recordTime",           (query, param) => query.Where(x => x.Request.CaptureDate < param.GetValue<DateTime>()) },
-            { "EQ_action",               (query, param) => query.Where(x => param.Values.Select(Enumeration.GetByDisplayName<EventAction>).Contains(x.Action)) },
-            { "EQ_bizLocation",          (query, param) => query.Where(x => param.Values.Contains(x.BusinessLocation)) },
-            { "EQ_bizStep",              (query, param) => query.Where(x => param.Values.Contains(x.BusinessStep)) },
-            { "EQ_disposition",          (query, param) => query.Where(x => param.Values.Contains(x.Disposition)) },
-            { "EQ_eventID",              (query, param) => query.Where(x => param.Values.Contains(x.EventId)) },
-            { "EQ_transformationID",     (query, param) => query.Where(x => param.Values.Contains(x.TransformationId)) },
-            { "EQ_readPoint",            (query, param) => query.Where(x => param.Values.Contains(x.ReadPoint)) },
-            { "EXISTS_errorDeclaration", (query, param) => query.Where(x => x.CorrectiveDeclarationTime.HasValue) },
-            { "EQ_errorReason",          (query, param) => query.Where(x => param.Values.Contains(x.CorrectiveReason)) },
-            { "EQ_correctiveEventID",    (query, param) => query.Where(x => x.CorrectiveEventIds.Any(ce => param.Values.Contains(ce.CorrectiveId))) },
-            { "WD_readPoint",            (query, param) => throw new EpcisException(ExceptionType.ImplementationException, "WD_readPoint parameter is not implemented") },
-            { "WD_bizLocation",          (query, param) => throw new EpcisException(ExceptionType.ImplementationException, "WD_bizLocation parameter is not implemented") },
-            { "EQ_requestId",            (query, param) => query.Where(x => param.Values.Select(int.Parse).Contains(x.Request.Id)) },
-            //{ "orderBy",                 (query, param) => query.Where(new OrderFilter { Field = Enumeration.GetByDisplayName<EpcisField>(param.GetValue<string>()) }) },
-            //{ "orderDirection",          (query, param) => query.Where(new OrderDirectionFilter { Direction = Enumeration.GetByDisplayName<OrderDirection>(param.GetValue<string>()) }) }
-        };
-        private static readonly IDictionary<string, Func<IQueryable<Event>, QueryParameter, IQueryable<Event>>> RegexParameters = new Dictionary<string, Func<IQueryable<Event>, QueryParameter, IQueryable<Event>>>
-        {
-            { "^EQ_(source|destination)_",  (query, param) => query.Where(x => x.SourceDests.Any(sd => sd.Id == param.GetParamNameValue('_', 3, 2) && sd.Direction == param.GetSourceDestinationType() && param.Values.Contains(sd.Type))) },
-            { "^EQ_bizTransaction_",        (query, param) => query.Where(x => x.Transactions.Any(t => t.Id == param.GetParamNameValue('_', 3, 2) && param.Values.Contains(t.Id))) },
-            //{ "^MATCH_",                    (query, param) => query.Where(new MatchEpcFilter { EpcType = param.GetMatchEpcTypes(), Values = param.Values.Select(x => x.Replace("*", "%")).ToArray() }) },
-            //{ "^(EQ|GT|LT|GE|LE)_quantity$",(query, param) => query.Where(new QuantityFilter { Operator = param.GetComparator(), Value = param.GetValue<double>() }) },
-            //{ "^EQ_INNER_ILMD_",            (query, param) => query.Where(new CustomFieldFilter { Field = param.GetField(FieldType.Ilmd, true), IsInner = true, Values = param.Values }) },
-            //{ "^EQ_ILMD_",                  (query, param) => query.Where(new CustomFieldFilter { Field = param.GetField(FieldType.Ilmd, false), IsInner = false, Values = param.Values }) },
-            //{ "^(GT|LT|GE|LE)_INNER_ILMD_", (query, param) => query.Where(new ComparisonCustomFieldFilter { Field = param.GetField(FieldType.Ilmd, true), Comparator = param.GetComparator(), IsInner = true, Value = param.GetComparisonValue()  }) },
-            //{ "^(GT|LT|GE|LE)_ILMD_",       (query, param) => query.Where(new ComparisonCustomFieldFilter { Field = param.GetField(FieldType.Ilmd, false), Comparator = param.GetComparator(), IsInner = false, Value = param.GetComparisonValue()  }) },
-            //{ "^EXISTS_INNER_ILMD_",        (query, param) => query.Where(new ExistCustomFieldFilter { Field = param.GetField(FieldType.Ilmd, true), IsInner = true }) },
-            //{ "^EXISTS_ILMD_",              (query, param) => query.Where(new ExistCustomFieldFilter { Field = param.GetField(FieldType.Ilmd, false), IsInner = false }) },
-            //{ "^EQ_INNER_",                 (query, param) => query.Where(new CustomFieldFilter { Field = param.GetField(FieldType.CustomField, true), IsInner = true, Values = param.Values }) },
-            //{ "^EQ_",                       (query, param) => query.Where(new CustomFieldFilter { Field = param.GetField(FieldType.CustomField, false), IsInner = false, Values = param.Values }) },
-            //{ "^(GT|LT|GE|LE)_INNER_",      (query, param) => query.Where(new ComparisonCustomFieldFilter { Field = param.GetField(FieldType.CustomField, true), Comparator = param.GetComparator(), IsInner = true, Value = param.GetComparisonValue() }) },
-            //{ "^(GT|LT|GE|LE)_",            (query, param) => query.Where(new ComparisonCustomFieldFilter { Field = param.GetField(FieldType.CustomField, false), Comparator = param.GetComparator(), IsInner = false, Value = param.GetComparisonValue() }) },
-            //{ "^EXISTS_INNER",              (query, param) => query.Where(new ExistCustomFieldFilter { Field = param.GetField(FieldType.CustomField, true), IsInner = true }) },
-            //{ "^EQATTR_",                   (query, param) => query.Where(new AttributeFilter { Field = param.GetAttributeField(), AttributeName = param.GetAttributeName(), Values = param.Values }) },
-            //{ "^HASATTR_",                  (query, param) => query.Where(new ExistsAttributeFilter { Field = param.GetAttributeField(), AttributeName = param.GetAttributeName()}) }
-        };
         private readonly EpcisContext _context;
         private int? _maxEventCount = default;
 
@@ -69,22 +23,28 @@ namespace FasTnT.Application.Queries.Poll
             _context = context;
         }
 
-        public string Name => "SimpleEventQuery";
+        public string Name => nameof(SimpleEventQuery);
         public bool AllowSubscription => true;
 
         public async Task<PollResponse> HandleAsync(IEnumerable<QueryParameter> parameters, CancellationToken cancellationToken)
         {
             var query = _context.Events.AsNoTracking();
 
+            // TODO: handle orderBy and orderDirection filters.
             foreach (var parameter in parameters)
             {
                 query = ApplyParameter(parameter, query);
             }
 
-            var eventIds = await query.Select(evt => evt.Id).ToListAsync(cancellationToken);
-            
-            CheckEventCountRestriction(eventIds);
-            
+            var eventIds = await query
+                .Select(evt => evt.Id)
+                .ToListAsync(cancellationToken);
+
+            if (_maxEventCount.HasValue && eventIds.Count > _maxEventCount)
+            {
+                throw new EpcisException(ExceptionType.QueryTooLargeException, $"Query returned more than the {_maxEventCount} events allowed.");
+            }
+
             var result = await _context.Events.AsSplitQuery()
                 .Include(x => x.Epcs)
                 .Include(x => x.SourceDests)
@@ -100,47 +60,60 @@ namespace FasTnT.Application.Queries.Poll
             };
         }
 
-        private IQueryable<Event> ApplyParameter(QueryParameter parameter, IQueryable<Event> query)
+        private IQueryable<Event> ApplyParameter(QueryParameter param, IQueryable<Event> query)
         {
-            if (IsSimpleParameter(parameter, out Func<IQueryable<Event>, QueryParameter, IQueryable<Event>> action) || IsRegexParameter(parameter, out action))
+            return param.Name switch
             {
-                try
-                {
-                    if (parameter.Name == "maxEventCount")
-                    {
-                        _maxEventCount = parameter.GetValue<int>();
-                    }
-
-                    return action(query, parameter);
-                }
-                catch (Exception ex)
-                {
-                    throw new EpcisException(ExceptionType.QueryParameterException, ex.Message);
-                }
-            }
-            
-            throw new NotImplementedException($"Query parameter unexpected or not implemented: '{parameter.Name}'");
+                "eventType"               => query.Where(x => param.Values.Select(Enumeration.GetByDisplayName<EventType>).Contains(x.Type)),
+                "eventCountLimit"         => query.Take(param.GetValue<int>()),
+                "maxEventCount"           => query.Take(1 + (_maxEventCount = param.GetValue<int>()) ?? default),
+                "GE_eventTime"            => query.Where(x => x.EventTime >= param.GetValue<DateTime>()),
+                "LT_eventTime"            => query.Where(x => x.EventTime < param.GetValue<DateTime>()),
+                "GE_recordTime"           => query.Where(x => x.Request.CaptureDate >= param.GetValue<DateTime>()),
+                "LT_recordTime"           => query.Where(x => x.Request.CaptureDate < param.GetValue<DateTime>()),
+                "EQ_action"               => query.Where(x => param.Values.Select(Enumeration.GetByDisplayName<EventAction>).Contains(x.Action)),
+                "EQ_bizLocation"          => query.Where(x => param.Values.Contains(x.BusinessLocation)),
+                "EQ_bizStep"              => query.Where(x => param.Values.Contains(x.BusinessStep)),
+                "EQ_disposition"          => query.Where(x => param.Values.Contains(x.Disposition)),
+                "EQ_eventID"              => query.Where(x => param.Values.Contains(x.EventId)),
+                "EQ_transformationID"     => query.Where(x => param.Values.Contains(x.TransformationId)),
+                "EQ_readPoint"            => query.Where(x => param.Values.Contains(x.ReadPoint)),
+                "EXISTS_errorDeclaration" => query.Where(x => x.CorrectiveDeclarationTime.HasValue),
+                "EQ_errorReason"          => query.Where(x => param.Values.Contains(x.CorrectiveReason)),
+                "EQ_correctiveEventID"    => query.Where(x => x.CorrectiveEventIds.Any(ce => param.Values.Contains(ce.CorrectiveId))),
+                "WD_readPoint"            => throw new EpcisException(ExceptionType.ImplementationException, "WD_readPoint parameter is not implemented"),
+                "WD_bizLocation"          => throw new EpcisException(ExceptionType.ImplementationException, "WD_bizLocation parameter is not implemented"),
+                "EQ_requestId"            => query.Where(x => param.Values.Select(int.Parse).Contains(x.Request.Id)),
+                var n when n.StartsWith("EQ_source_")         => query.Where(x => x.SourceDests.Any(sd => sd.Id == param.GetParamNameValue('_', 3, 2) && sd.Direction == SourceDestinationType.Source && param.Values.Contains(sd.Type))),
+                var n when n.StartsWith("EQ_destination_")    => query.Where(x => x.SourceDests.Any(sd => sd.Id == param.GetParamNameValue('_', 3, 2) && sd.Direction == SourceDestinationType.Destination && param.Values.Contains(sd.Type))),
+                var n when n.StartsWith("EQ_bizTransaction_") => query.Where(x => x.Transactions.Any(t => t.Id == param.GetParamNameValue('_', 3, 2) && param.Values.Contains(t.Id))),
+                var n when n.StartsWith("MATCH_")             => ApplyMatchParameter(param, query),
+                //{ "^(EQ|GT|LT|GE|LE)_quantity$",(query, param) => query.Where(new QuantityFilter { Operator = param.GetComparator(), Value = param.GetValue<double>() }) },
+                var n when n.StartsWith("EQ_INNER_ILMD_")     => query.Where(x => x.CustomFields.Any(f => f.Type == FieldType.Ilmd && f.Parent != null && param.Values.Contains(f.TextValue) && f.Name == param.GetField(FieldType.Ilmd, true).Name && f.Namespace == param.GetField(FieldType.Ilmd, true).Namespace)),
+                var n when n.StartsWith("EQ_ILMD_")           => query.Where(x => x.CustomFields.Any(f => f.Type == FieldType.Ilmd && f.Parent == null && param.Values.Contains(f.TextValue) && f.Name == param.GetField(FieldType.Ilmd, false).Name && f.Namespace == param.GetField(FieldType.Ilmd, false).Namespace)),
+                //{ "^(GT|LT|GE|LE)_INNER_ILMD_", (query, param) => query.Where(new ComparisonCustomFieldFilter { Field = param.GetField(FieldType.Ilmd, true), Comparator = param.GetComparator(), IsInner = true, Value = param.GetComparisonValue()  }) },
+                //{ "^(GT|LT|GE|LE)_ILMD_",       (query, param) => query.Where(new ComparisonCustomFieldFilter { Field = param.GetField(FieldType.Ilmd, false), Comparator = param.GetComparator(), IsInner = false, Value = param.GetComparisonValue()  }) },
+                var n when n.StartsWith("EXISTS_INNER_ILMD_") => query.Where(x => x.CustomFields.Any(f => f.Type == FieldType.Ilmd && f.Parent != null && f.Name == param.GetField(FieldType.Ilmd, true).Name && f.Namespace == param.GetField(FieldType.Ilmd, true).Namespace)),
+                var n when n.StartsWith("EXISTS_ILMD_")       => query.Where(x => x.CustomFields.Any(f => f.Type == FieldType.Ilmd && f.Parent == null && f.Name == param.GetField(FieldType.Ilmd, false).Name && f.Namespace == param.GetField(FieldType.Ilmd, false).Namespace)),
+                var n when n.StartsWith("EQ_INNER_")          => query.Where(x => x.CustomFields.Any(f => f.Parent != null && f.Name == param.GetField(FieldType.CustomField, true).Name && f.Namespace == param.GetField(FieldType.CustomField, true).Namespace && param.Values.Contains(f.TextValue))),
+                var n when n.StartsWith("EQ_")                => query.Where(x => x.CustomFields.Any(f => f.Parent == null && f.Name == param.GetField(FieldType.CustomField, false).Name && f.Namespace == param.GetField(FieldType.CustomField, false).Namespace && param.Values.Contains(f.TextValue))),
+                //{ "^(GT|LT|GE|LE)_INNER_",      (query, param) => query.Where(new ComparisonCustomFieldFilter { Field = param.GetField(FieldType.CustomField, true), Comparator = param.GetComparator(), IsInner = true, Value = param.GetComparisonValue() }) },
+                //{ "^(GT|LT|GE|LE)_",            (query, param) => query.Where(new ComparisonCustomFieldFilter { Field = param.GetField(FieldType.CustomField, false), Comparator = param.GetComparator(), IsInner = false, Value = param.GetComparisonValue() }) },
+                var n when n.StartsWith("EXISTS_INNER_")      => query.Where(x => x.CustomFields.Any(f => f.Parent != null && f.Name == param.GetField(FieldType.CustomField, true).Name && f.Namespace == param.GetField(FieldType.CustomField, true).Namespace)),
+                //{ "^EQATTR_",                   (query, param) => query.Where(new AttributeFilter { Field = param.GetAttributeField(), AttributeName = param.GetAttributeName(), Values = param.Values }) },
+                //{ "^HASATTR_",                  (query, param) => query.Where(new ExistsAttributeFilter { Field = param.GetAttributeField(), AttributeName = param.GetAttributeName()}) }
+                _ => throw new EpcisException(ExceptionType.QueryParameterException, $"Parameter is not implemented: {param.Name}")
+            };
         }
 
-        private static bool IsSimpleParameter(QueryParameter parameter, out Func<IQueryable<Event>, QueryParameter, IQueryable<Event>> action)
+        private static IQueryable<Event> ApplyMatchParameter(QueryParameter param, IQueryable<Event> query)
         {
-            return SimpleParameters.TryGetValue(parameter.Name, out action);
-        }
-
-        private static bool IsRegexParameter(QueryParameter parameter, out Func<IQueryable<Event>, QueryParameter, IQueryable<Event>> action)
-        {
-            var matchingRegex = RegexParameters.FirstOrDefault(x => Regex.Match(parameter.Name, x.Key, RegexOptions.Singleline).Success);
-            action = matchingRegex.Value;
-
-            return matchingRegex.Key != default;
-        }
-
-        private void CheckEventCountRestriction(IEnumerable<long> result)
-        {
-            if (_maxEventCount.HasValue && result.Count() > _maxEventCount)
+            for (int i = 0; i < param.Values.Length; i++)
             {
-                throw new EpcisException(ExceptionType.QueryTooLargeException, $"Query returned more than the {_maxEventCount} events allowed.");
+                query = query.Where(x => x.Epcs.Any(e => param.GetMatchEpcTypes().Contains(e.Type) && EF.Functions.Like(e.Id, param.Values[i].Replace("*", "%"))));
             }
+
+            return query;
         }
     }
 }
