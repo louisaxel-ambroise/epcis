190f01ae443e65e90a643f3bfc9a03c6d23638a7
Louis-Axel Ambroise <ambroise.la@pm.me>
2021-04-15 07:47:29 +0200

    Fix host issues

--- FasTnT.Host/Features/v1_2/Epcis12Module.cs
+++ FasTnT.Host/Features/v1_2/Epcis12Module.cs
@@ -4,7 +4,7 @@ namespace FasTnT.Host.Features.v1_2
 {
     public abstract class Epcis12Module : CarterModule
     {
-        public Epcis12Module() : base("v1_2/")
+        protected Epcis12Module() : base("v1_2/")
         {
         }
     }
