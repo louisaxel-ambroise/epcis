8dec83f742054119d356bde9f8ae0fcfbecfb918
Louis-Axel Ambroise <ambroise.la@pm.me>
2021-06-02 15:33:48 +0200

    Update validation

--- FasTnT.Domain/Infrastructure/Behaviors/CommandValidationBehavior.cs
+++ FasTnT.Domain/Infrastructure/Behaviors/CommandValidationBehavior.cs
@@ -1,7 +1,6 @@
 ﻿using FasTnT.Domain.Exceptions;
 using FluentValidation;
 using MediatR;
-using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -12,12 +11,10 @@ namespace FasTnT.Domain.Infrastructure.Behaviors
 	public class CommandValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
 	{
 		private readonly IEnumerable<IValidator<TRequest>> _validators;
-		private readonly ILogger<CommandValidationBehavior<TRequest, TResponse>> _logger;
 
-		public CommandValidationBehavior(IEnumerable<IValidator<TRequest>> validators, ILogger<CommandValidationBehavior<TRequest, TResponse>> logger)
+		public CommandValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
 		{
 			_validators = validators;
-			_logger = logger;
 		}
 
 		public Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
@@ -32,8 +29,6 @@ namespace FasTnT.Domain.Infrastructure.Behaviors
 
 			if (failures.Length > 0)
 			{
-				_logger.LogError($"CommandValidationBehavior: {string.Join(";", failures)}");
-
 				throw new EpcisException(ExceptionType.ValidationException, string.Join(", ", failures));
 			}
 
