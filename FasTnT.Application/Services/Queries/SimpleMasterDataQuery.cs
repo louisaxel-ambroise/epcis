8d9b5d14e09555a1314fc6bf7d5de81814aa6381
Louis-Axel Ambroise <ambroise.la@pm.me>
2021-09-05 18:29:27 +0200

    Implement simplemasterdata query parameters

--- FasTnT.Application/Services/Queries/SimpleMasterDataQuery.cs
+++ FasTnT.Application/Services/Queries/SimpleMasterDataQuery.cs
@@ -1,8 +1,11 @@
 ﻿using FasTnT.Application.Services;
+using FasTnT.Domain.Exceptions;
 using FasTnT.Domain.Model;
 using FasTnT.Domain.Queries.Poll;
+using FasTnT.Domain.Utils;
 using FasTnT.Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -13,6 +16,7 @@ namespace FasTnT.Application.Queries.Poll
     public class SimpleMasterDataQuery : IEpcisQuery
     {
         private readonly EpcisContext _context;
+        private int? _maxEventCount;
 
         public SimpleMasterDataQuery(EpcisContext context)
         {
@@ -25,14 +29,42 @@ namespace FasTnT.Application.Queries.Poll
         // TODO: apply parameters
         public async Task<PollResponse> HandleAsync(IEnumerable<QueryParameter> parameters, CancellationToken cancellationToken)
         {
-            var query = _context.MasterData.AsNoTracking();
+            var query = _context.MasterData.AsSplitQuery().AsNoTrackingWithIdentityResolution();
 
-            var result = await query.AsSplitQuery().AsNoTrackingWithIdentityResolution()
-                .Include(x => x.Attributes)
-                .ThenInclude(x => x.Fields)
-                .ToListAsync(cancellationToken);
+            foreach(var parameter in parameters)
+            {
+                query = ApplyParameter(parameter, query);
+            }
+
+            var result = await query.ToListAsync(cancellationToken);
+
+            if (_maxEventCount.HasValue && result.Count > _maxEventCount)
+            {
+                throw new EpcisException(ExceptionType.QueryTooLargeException, $"Query returned more than the {_maxEventCount} elements allowed.");
+            }
 
             return new(Name) { VocabularyList = result };
+        }
+
+        private IQueryable<MasterData> ApplyParameter(QueryParameter param, IQueryable<MasterData> query)
+        {
+            return param.Name switch
+            {
+                "maxElementCount" => ParseMaxElementCount(param, query),
+                "vocabularyName" => query.Where(x => x.Type == param.Value()),
+                "EQ_name" => query.Where(x => x.Id == param.Value()),
+                "includeAttributes" => query.Include(x => x.Attributes).ThenInclude(x => x.Fields),
+                "attributeNames" => query.Include(x => x.Attributes.Where(a => param.Values.Contains(a.Id))).ThenInclude(x => x.Fields),
+                "includeChildren" => query,
+                "HASATTR" => query.Where(x => x.Attributes.Any(a => a.Id == param.Value())),
+                _ => throw new EpcisException(ExceptionType.QueryParameterException, $"Parameter is not implemented: {param.Name}")
+            };
+        }
+        private IQueryable<MasterData> ParseMaxElementCount(QueryParameter param, IQueryable<MasterData> query)
+        {
+            _maxEventCount = param.GetIntValue();
+
+            return query.Take(1 + _maxEventCount.Value);
         }
     }
 }
