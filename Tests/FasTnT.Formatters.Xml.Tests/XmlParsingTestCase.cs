a7b4131a25da924ac50c30308f7845e355647467
Louis-Axel Ambroise <ambroise.la@pm.me>
2021-09-08 08:41:04 +0200

    Add first xml parser tests

---
+++ Tests/FasTnT.Formatters.Xml.Tests/XmlParsingTestCase.cs
@@ -0,0 +1,17 @@
+﻿using FasTnT.Formatter.Xml.Parsers;
+using System.Reflection;
+using System.Xml.Linq;
+
+namespace FasTnT.Formatters.Xml.Tests
+{
+    public abstract class XmlParsingTestCase
+    {
+        protected XDocument ParseResource(string resourceName)
+        {
+            var manifest = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName);
+            using var resourceStream = XmlDocumentParser.Instance.ParseAsync(manifest, default);
+         
+            return resourceStream.Result;
+        }
+    }
+}
