a1de8d756d5fd8cfe52dec1bbcd808d439781b90
Louis-Axel Ambroise <ambroise.la@pm.me>
2021-10-22 14:41:06 +0200

    Add Query/Command handlers tests

---
+++ Tests/FasTnT.Application.Tests/WhenHandlingGetSubscriptionIdsQuery.cs
@@ -0,0 +1,54 @@
+﻿using FasTnT.Application.Queries.GetQueryNames;
+using FasTnT.Application.Tests.Context;
+using FasTnT.Domain.Queries.GetSubscriptionIds;
+using FasTnT.Infrastructure.Database;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace FasTnT.Application.Tests
+{
+    [TestClass]
+    public class WhenHandlingGetSubscriptionIdsQuery
+    {
+        public readonly static EpcisContext Context = Tests.Context.TestContext.GetContext(nameof(Tests.WhenHandlingGetSubscriptionIdsQuery));
+
+        [ClassInitialize]
+        public static void Initialize(Microsoft.VisualStudio.TestTools.UnitTesting.TestContext _)
+        {
+            Context.Subscriptions.Add(new Domain.Model.Subscription
+            {
+                Name = "SubscriptionTest",
+                QueryName = "TestQuery"
+            });
+            Context.Subscriptions.Add(new Domain.Model.Subscription
+            {
+                Name = "OtherSubscription",
+                QueryName = "OtherQuery"
+            });
+
+            Context.SaveChanges();
+        }
+
+        [TestMethod]
+
+
+
+        public void ItShouldReturnTheListOfExistingSubscriptionIdsForTheSpecifiedRequest()
+        {
+            var handler = new GetSubscriptionIdsQueryHandler(Context);
+            var result = handler.Handle(new GetSubscriptionIdsQuery { QueryName = "TestQuery" }, default).Result;
+            
+            Assert.AreEqual(1, result.SubscriptionIDs.Count());
+            Assert.AreEqual("SubscriptionTest", result.SubscriptionIDs.First());
+        }
+
+        [TestMethod]
+        public void ItShouldReturnAnEmptyListWhenNoSubscriptionMatch()
+        {
+            var handler = new GetSubscriptionIdsQueryHandler(Context);
+            var result = handler.Handle(new GetSubscriptionIdsQuery { QueryName = "UnknownQuery" }, default).Result;
+            
+            Assert.AreEqual(0, result.SubscriptionIDs.Count());
+        }
+    }
+}
