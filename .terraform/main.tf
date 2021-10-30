6b2bb319e19ad395d0a59a86b71fec77d48775a2
Louis-Axel Ambroise <ambroise.la@pm.me>
2021-10-30 14:38:35 +0200

    Add availability test

--- .terraform/main.tf
+++ .terraform/main.tf
@@ -80,6 +80,27 @@ resource "azurerm_application_insights" "api_appi" {
   tags                = local.tags
 }
 
+resource "azurerm_application_insights_web_test" "api_webtest" {
+  name                    = "${local.base_resource_name}-api-appinsights-webtest"
+  location                = var.location
+  resource_group_name     = azurerm_resource_group.fastnt_main.name
+  application_insights_id = azurerm_application_insights.api_appi.id
+  kind                    = "ping"
+  frequency               = 900
+  timeout                 = 60
+  enabled                 = true
+  geo_locations           = ["emea-fr-pra-edge", "emea-nl-ams-azr", "emea-ru-msa-edge"]
+
+  configuration = <<XML
+<WebTest Name="WebTest1" Id="ABD48585-0831-40CB-9069-682EA6BB3583" Enabled="True" CssProjectStructure="" CssIteration="" Timeout="0" WorkItemIds="" xmlns="http://microsoft.com/schemas/VisualStudio/TeamTest/2010" Description="" CredentialUserName="" CredentialPassword="" PreAuthenticate="True" Proxy="default" StopOnError="False" RecordedResultFile="" ResultsLocale="">
+  <Items>
+    <Request Method="GET" Guid="a5f10126-e4cd-570d-961c-cea43999a200" Version="1.1" Url="https://${local.base_resource_name}-api.azurewebsites.net" ThinkTime="0" Timeout="300" ParseDependentRequests="True" FollowRedirects="True" RecordResult="True" Cache="False" ResponseTimeGoal="0" Encoding="utf-8" ExpectedHttpStatusCode="200" ExpectedResponseUrl="" ReportingName="" IgnoreHttpStatusCode="False" />
+  </Items>
+</WebTest>
+XML
+}
+
+
 resource "azurerm_app_service" "api_app" {
   name                = "${local.base_resource_name}-api"
   location            = var.location
