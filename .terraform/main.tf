6ad00241f72c4566ef330f10742b4200b5aa4707
Louis-Axel Ambroise <ambroise.la@pm.me>
2021-10-30 10:23:30 +0200

    Add terraform script

---
+++ .terraform/main.tf
@@ -0,0 +1,110 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+  backend "azurerm" {
+    storage_account_name = ""
+    access_key           = ""
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+
+  required_version = ">= 1.0.0"
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+}
+
+locals {
+  tags = {
+    Environment = upper(var.environment)
+  }
+  base_resource_name = lower("fastnt-${var.environment}")
+}
+
+resource "azurerm_resource_group" "fastnt_main" {
+  name     = local.base_resource_name
+  location = var.location
+  tags     = local.tags
+}
+
+resource "azurerm_storage_account" "assetstracking_storage" {
+  name                     = replace("${local.base_resource_name}", "-", "")
+  location                 = var.location
+  resource_group_name      = azurerm_resource_group.fastnt_main.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  tags                     = local.tags
+}
+
+resource "azurerm_mssql_server" "sql-db-server" {
+  name                         = "${local.base_resource_name}-sql-server"
+  resource_group_name          = azurerm_resource_group.fastnt_main.name
+  location                     = var.location
+  version                      = "12.0"
+  administrator_login          = var.database_username
+  administrator_login_password = var.database_password
+  minimum_tls_version          = "1.2"
+  tags                         = local.tags
+}
+
+resource "azurerm_mssql_database" "sqldb" {
+  name                        = "${local.base_resource_name}-sql-db"
+  server_id                   = azurerm_mssql_server.sql-db-server.id
+  collation                   = "SQL_Latin1_General_CP1_CI_AS"
+  max_size_gb                 = var.sql_max_size_gb
+  sku_name                    = var.sql_sku
+  tags                        = local.tags
+}
+
+resource "azurerm_mssql_firewall_rule" "sql-db-server-allow-azure" {
+  name = "AllowAccessToAzureServices"
+  server_id = azurerm_mssql_server.sql-db-server.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address = "0.0.0.0"
+}
+
+resource "azurerm_app_service_plan" "plan" {
+  name                = "${local.base_resource_name}-app-plan"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.assetstracking_main.name
+  sku {
+    tier = var.app_tier
+    size = var.app_size
+  }
+  tags = local.tags
+}
+
+resource "azurerm_application_insights" "api_appi" {
+  name                = "${local.base_resource_name}-api-appinsights"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.fastnt_main.name
+  application_type    = "web"
+  tags                = local.tags
+}
+
+resource "azurerm_app_service" "api_app" {
+  name                = "${local.base_resource_name}-api"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.fastnt_main.name
+  app_service_plan_id = azurerm_app_service_plan.plan.id
+  tags                = local.tags
+  app_settings = {
+    "APPINSIGHTS_INSTRUMENTATIONKEY" = "${azurerm_application_insights.api_appi.instrumentation_key}"
+    "APPLICATIONINSIGHTS_CONNECTION_STRING" : "${azurerm_application_insights.api_appi.connection_string}"
+    "ApplicationInsightsAgent_EXTENSION_VERSION" : "~2"
+    "ConnectionStrings:FasTnT.Database" : "Server=tcp:${azurerm_mssql_server.sql-db-server.fully_qualified_domain_name},1433;Initial Catalog=${local.base_resource_name}-sql-db;Persist Security Info=False;User ID=${azurerm_mssql_server.sql-db-server.administrator_login};Password=${azurerm_mssql_server.sql-db-server.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  }
+}
+
+resource "azurerm_app_service_source_control" "api_source_control" {
+  app_id   = azurerm_app_service.api_app.id
+  repo_url = "https://github.com/FasTnT/epcis-ef-core"
+  branch   = "main"
+}
---
+++ .terraform/variables.tf
@@ -0,0 +1,50 @@
+variable "subscription_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "environment" {
+  type    = string
+  default = "dev"
+}
+
+variable "location" {
+  type    = string
+  default = "westeurope"
+}
+
+variable "database_username" {
+  type        = string
+  description = "Administrator username for the database server"
+  sensitive   = true
+}
+
+variable "database_password" {
+  type        = string
+  description = "Administrator password for the database server"
+  sensitive   = true
+}
+
+variable "app_tier" {
+  type = string
+  description = "The tier for the app service plan. Defaults to Basic"
+  default = "Basic"
+}
+
+variable "app_size" {
+  type = string
+  description = "The size for the app service plan. Defaults to B1"
+  default = "B1"
+}
+
+variable "sql_max_size_gb" {
+  type        = string
+  description = "Max size of the database in GB. Defaults to 2."
+  default     = "2"
+}
+
+variable "sql_sku" {
+  type        = string
+  description = "SKU for the database. Defaults to Basic."
+  default     = "Basic"
+}
