ea097a925367efdd45bce54f265bd3f0ff74d8fd
Louis-Axel Ambroise <ambroise.la@pm.me>
2021-11-09 16:55:31 +0100

    Add basic log + remove old files

--- FasTnT.Domain/Infrastructure/Behaviors/CommandValidationBehavior.cs
+++ FasTnT.Domain/Infrastructure/Behaviors/CommandValidationBehavior.cs
@@ -1,6 +1,7 @@
 ﻿using FasTnT.Domain.Exceptions;
 using FluentValidation;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace FasTnT.Domain.Infrastructure.Behaviors;
 
@@ -28,5 +29,29 @@ public class CommandValidationBehavior<TRequest, TResponse> : IPipelineBehavior<
         }
 
         return next();
+    }
+}
+
+public class CommandLoggerBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+{
+    private readonly ILogger _logger;
+
+    public CommandLoggerBehavior(ILogger<CommandLoggerBehavior<TRequest, TResponse>> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+    {
+        _logger.LogInformation("Handling {Name}", typeof(TRequest).Name);
+
+        try
+        {
+            return next();
+        }
+        finally
+        {
+            _logger.LogInformation("Handled {Name}", typeof(TRequest).Name);
+        }
     }
 }
--- FasTnT.Infrastructure/Configuration/IdentityGenerator.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FasTnT.Infrastructure.Configuration
-{
-    public class IdentityGenerator
-    {
-        private int _lastValue;
-
-        public int NextValue => _lastValue += 1;
-    }
-}
