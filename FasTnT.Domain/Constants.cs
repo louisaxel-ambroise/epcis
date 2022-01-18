8abc5b7197af279ebfe6ad5f1d0a976e925e216b
Louis-Axel Ambroise <ambroise.la@pm.me>
2022-01-18 14:50:47 +0100

    Bump version to 1.0.0

--- FasTnT.Domain/Constants.cs
+++ FasTnT.Domain/Constants.cs
@@ -3,5 +3,5 @@
 public static class Constants
 {
     public static int MaxEventsReturnedInQuery { get; set; } = 20_000;
-    public static string VendorVersion { get; set; } = "0.5";
+    public static string VendorVersion { get; set; } = "1.0.0";
 }
