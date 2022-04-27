4a0922133d6fe8b97c510d77f2ae3e8a0bee8727
Louis-Axel Ambroise <ambroise.la@pm.me>
2022-04-27 12:35:07 +0200

    Remove cursor from pending subscription trigger for Npgsql (#153)
    * Remove cursor from pending subscription trigger for Npgsql
    
    * Update migration for Npgsql

--- FasTnT.Migrations.Npgsql/Migrations/20220409104541_AddSubscriptionTriggers.cs
+++ FasTnT.Migrations.Npgsql/Migrations/20220409104541_AddSubscriptionTriggers.cs
@@ -14,22 +14,19 @@ namespace FasTnT.Migrations.Npgsql.Migrations
     COST 100
     VOLATILE NOT LEAKPROOF
 AS $BODY$
-    DECLARE
-        c_subscription CURSOR FOR SELECT * FROM ""Subscription"".""Subscription"";
-            r_subscription ""Subscription"".""Subscription""%ROWTYPE;
-            BEGIN
-                FOR r_subscription IN c_subscription LOOP
-            INSERT INTO ""Subscriptions"".""Pendingrequest""(""SubscriptionId"", ""RequestId"") VALUES(r_subscription.id, NEW.id);
-            END LOOP;
+    BEGIN
+        INSERT INTO ""Subscription"".""PendingRequest""(""SubscriptionId"", ""RequestId"")
+        SELECT s.""Id"", NEW.""Id""
+        FROM ""Subscription"".""Subscription"" s;
 
-            RETURN NULL;
-            END;
+        RETURN NULL;
+    END;
 $BODY$;");
             migrationBuilder.Sql(@"CREATE TRIGGER add_pending_request AFTER INSERT ON ""Epcis"".""Request""
 FOR EACH ROW EXECUTE PROCEDURE ""Subscription"".update_subscriptions();");
         }
 
-        protected override void Down(MigrationBuilder migrationBuilder)
+        protected override void Down(MigrationBuilder migrationBuilder) 
         {
             migrationBuilder.Sql(@"DROP TRIGGER IF EXISTS add_pending_request ON ""Epcis"".""Request"" CASCADE;");
             migrationBuilder.Sql(@"DROP FUNCTION ""Subscription"".update_subscriptions();");
