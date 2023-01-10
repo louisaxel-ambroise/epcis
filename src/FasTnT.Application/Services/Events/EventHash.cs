1d8238ab9ab937d97a91f6d12a7bf02c00e35396
Louis-Axel Ambroise <ambroise.la@pm.me>
2023-01-10 15:08:59 +0100

    Set UUID v4 eventID if empty

--- src/FasTnT.Application/Services/Events/EventHash.cs
+++ src/FasTnT.Application/Services/Events/EventHash.cs
@@ -4,8 +4,8 @@ namespace FasTnT.Application.Services.Events;
 
 public static class EventHash
 {
-    public static string Compute(Event evt)
+    public static string Compute(Event _)
     {
-        throw new NotImplementedException();
+        return $"urn:uuid:{Guid.NewGuid()}";
     }
 }
