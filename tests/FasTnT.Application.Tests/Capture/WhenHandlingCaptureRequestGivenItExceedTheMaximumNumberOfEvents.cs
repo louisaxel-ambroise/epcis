809127fe472836ea3448479e09006c395559a1af
Louis-Axel Ambroise <ambroise.la@pm.me>
2023-03-17 17:53:29 +0100

    Tests

---
+++ tests/FasTnT.Application.Tests/Capture/WhenHandlingCaptureRequestGivenItExceedTheMaximumNumberOfEvents.cs
@@ -0,0 +1,36 @@
+﻿using FasTnT.Application.Database;
+using FasTnT.Application.Services.Users;
+using FasTnT.Application.Tests.Context;
+using FasTnT.Application.Handlers;
+using FasTnT.Domain.Enumerations;
+using FasTnT.Domain.Model;
+using FasTnT.Domain.Model.Events;
+using FasTnT.Domain;
+using FasTnT.Domain.Exceptions;
+
+namespace FasTnT.Application.Tests.Capture;
+
+[TestClass]
+public class WhenHandlingCaptureRequestGivenItExceedTheMaximumNumberOfEvents
+{
+    readonly static EpcisContext Context = EpcisTestContext.GetContext(nameof(WhenHandlingCaptureRequestGivenItExceedTheMaximumNumberOfEvents));
+    readonly static ICurrentUser UserContext = new TestCurrentUser();
+    readonly static TestSubscriptionListener SubscriptionListener = new();
+
+    [ClassInitialize]
+    public static void Initialize(TestContext _)
+    {
+        Constants.Instance = new Constants() { MaxEventsCapturePerCall = 1 };
+    }
+
+    [TestMethod]
+    public void ItShouldThrowAnExceptionAnNotCaptureTheRequest()
+    {
+        var handler = new CaptureHandler(Context, UserContext, SubscriptionListener);
+        var request = new Request { SchemaVersion = "1.0", Events = new() { new Event { Type = EventType.ObjectEvent }, new Event { Type = EventType.ObjectEvent } } };
+        
+        Assert.ThrowsExceptionAsync<EpcisException>(() => handler.StoreAsync(request, default));
+        Assert.AreEqual(0, Context.Set<Request>().Count());
+        Assert.IsFalse(SubscriptionListener.IsTriggered("stream"));
+    }
+}
