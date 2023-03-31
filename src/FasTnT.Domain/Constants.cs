ec2169c69cd47b09ebe2ac38aea606a214f47448
Louis-Axel Ambroise <ambroise.la@pm.me>
2023-03-31 17:55:52 +0200

    Version

--- src/FasTnT.Domain/Constants.cs
+++ src/FasTnT.Domain/Constants.cs
@@ -6,5 +6,5 @@ public sealed class Constants
 
     public int MaxEventsCapturePerCall { get; init; } = 500;
     public int MaxEventsReturnedInQuery { get; init; } = 20_000;
-    public Version VendorVersion { get; init; } = Version.Parse("2.4.3");
+    public Version VendorVersion { get; init; } = Version.Parse("2.4.2");
 }
